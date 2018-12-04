52d101add8521cdb7bcf90f6f1b3ba85fc2cf337
github username <github email address>
2018-12-04 16:17:55 -0500

    create song table

---
+++ db/migrate/20181204210148_create_songs.rb
@@ -0,0 +1,14 @@
+class CreateSongs < ActiveRecord::Migration[5.0]
+  def change
+    #create first table for Song Class
+    create_table :songs do |t|
+      t.string :title
+      t.float :duratiion
+      t.boolean :inward
+      t.boolean :outward
+      t.boolean :onward
+      t.boolean :vocal
+      t.boolean :instrumental
+    end
+  end
+end
