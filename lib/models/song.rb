4c9631ac95b62cc9babdbd629c6cfa950b18dcfb
susan <bluenote@outlook.com>
2018-12-05 13:18:44 -0500

    nothing

--- lib/models/song.rb
+++ lib/models/song.rb
@@ -2,10 +2,27 @@ class Song < ActiveRecord::Base
 	has_many :song_categories
 	has_many :categories, through: :song_categories
 
+	# def song_categories
+	# 	SongCategory.all.select do |song_category|
+	# 		song_category.song == self
+	# 	end
+	# end
+
+	# def categories
+	# 	self.song_categories.map do |song_category|
+	# 		song_category.category
+	# 	end
+	# end
+
 	def category_names
 		self.categories.map do |category|
 			category.description
 		end
 	end
-#def find_by
+
+ #  def find_by(category1, category2)
+ #  	Song.all.include? |song|
+ #      song.category_names == category1 && song.category_names == category2
+ #     end
+ #  end
 end
